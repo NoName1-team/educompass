6f2e7dad4a20309da2220ccc760da7e152e036a1
Nazrulloh <iamnazrulloh@gmail.com>
2024-09-22 03:31:09 +0500

    User sides started

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class SignUpForm(forms.ModelForm):
+    phone = forms.CharField(
+        max_length=15,
+        widget=forms.TextInput(attrs={'placeholder': 'XX XXX XX XX'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'phone']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Ism'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Familiya'}),
+        }
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.username = self.cleaned_data['phone']
+        if commit:
+            user.save()
+        return user
